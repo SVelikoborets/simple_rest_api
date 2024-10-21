7e11860de793ce51bca0da6425a45c6abdb76021
S.Velikoborets <safiya.soboleva.97@mail.ru>
2024-10-21 08:44:57 +0300

    Ðstep four: added caching and cache reset

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ $dataFile = 'data.txt';
 $logFile = 'log.txt';
 $largeLogFile = 'large_requests_log.txt';
 $maxDataSize = 10 * 1024 * 1024;
+$cacheLifetime = 3600;
+$cacheFile = 'cache.json';
 function getClientIP()
 {
     return $_SERVER['REMOTE_ADDR'];
@@ -38,6 +40,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         '| Length:' . strlen($data['text']) . "\n";
 
     file_put_contents($logFile, $newLog, FILE_APPEND );
+
+    // Сброс кэша
+    if (file_exists($cacheFile)) {
+        unlink($cacheFile);
+    }
+
     echo json_encode(['success' => 'Данные записаны']);
     exit;
 }
@@ -58,19 +66,33 @@ if($_SERVER['REQUEST_METHOD'] === 'GET') {
         exit;
     }
 
-    $limit = isset($_GET['limit']) ? (int) $_GET['limit'] : 100;
-    $offset = isset($_GET['offset']) ? (int) $_GET['offset'] : 0;
+    if (file_exists($cacheFile) && filemtime($cacheFile) > filemtime($dataFile) - $cacheLifetime) {
+    // Чтение данных из кэша
+        $cacheData = json_decode(file_get_contents($cacheFile), true);
+        $data = implode("\n", $cacheData);
+    } else {
+    // Чтение данных из файла
+        $lines = file($dataFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-// Чтение данных из файла
-    $lines = file($dataFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    // Получение параметров limit и offset из GET-запроса
+        $limit = isset($_GET['limit']) ? (int) $_GET['limit'] : 100;
+        $offset = isset($_GET['offset']) ? (int) $_GET['offset'] : 0;
 
-// Обрезка данных по параметрам limit и offset
-    $data = array_slice($lines, $offset, $limit);
+    // Обрезка данных по параметрам limit и offset
+        $dataPart = array_slice($lines, $offset, $limit);
 
-    echo json_encode(['data' => implode(PHP_EOL, $data)]);
+    // Сохранение данных в кэш
+        file_put_contents($cacheFile, json_encode($dataPart,  JSON_UNESCAPED_UNICODE));
+
+        $data = implode("\n", $dataPart);
+    }
+
+    echo json_encode(['data' => $data], JSON_UNESCAPED_UNICODE);
     exit;
 }
 
 http_response_code(405);
-echo json_encode(['error' => 'Неверный метод']);
+echo json_encode(['error' => 'Некорректный метод']);
 exit;
+
+
